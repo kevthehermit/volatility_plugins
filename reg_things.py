e1a786fca5e5819192b65c8fa9247ec50df57748
KevTheHermit <kevthehermit@gmail.com>
2016-09-22 14:20:18 +0100

    Last writ time

--- reg_things.py
+++ reg_things.py
@@ -7,7 +7,6 @@
 Note: All of the information being queried may not be present
 """
 
-import inspect
 import volatility.addrspace as addrspace
 import volatility.debug as debug
 import volatility.obj as obj
@@ -20,8 +19,8 @@ import volatility.utils as utils
 from datetime import datetime
 import struct
 
-class RegThings(common.AbstractWindowsCommand):
-    "Print some things from a registry"
+class USBSTOR(common.AbstractWindowsCommand):
+    "Parse USB Data from the Registry"
 
 
     def __init__(self, config, *args, **kwargs):
@@ -45,28 +44,76 @@ class RegThings(common.AbstractWindowsCommand):
         # Store teh results in a dict
         results = {}
         results['subkeys'] = []
+        results['USB_DEVICES'] = []
 
+
+        USB_PATH = '{0}\\Enum\\USB'.format(currentcs)
+        usb_key = self.regapi.reg_get_key('SYSTEM', USB_PATH)
         USB_STOR_PATH = '{0}\\Enum\\USBSTOR'.format(currentcs)
-        key = self.regapi.reg_get_key('SYSTEM', USB_STOR_PATH)
+        usb_stor_key = self.regapi.reg_get_key('SYSTEM', USB_STOR_PATH)
 
-        if key == None:
+
+        if usb_stor_key == None:
             results['exists'] = False
         else:
             results['exists'] = True
 
-        sub_keys = self.regapi.reg_get_all_subkeys('SYSTEM', USB_STOR_PATH, given_root=key)
+        sub_keys = self.regapi.reg_get_all_subkeys('SYSTEM', USB_STOR_PATH, given_root=usb_stor_key)
         for k in sub_keys:
-            debug.info("Vendor / Brand / Rev: {0}".format(str(k.Name)))
+            # Theses are grouped by vender
+            #debug.info("Vendor / Brand / Rev: {0}".format(str(k.Name)))
+
+            disk, vendor, product, rev = str(k.Name).split('&')
+            vendor = vendor.split('_', 1)[-1]
+            product = product.split('_', 1)[-1]
+            rev = rev.split('_', 1)[-1]
+
             results['subkeys'].append(str(k.Name))
             usb_devs = self.regapi.reg_get_all_subkeys('SYSTEM', k, given_root=k)
             for dev in usb_devs:
-                debug.info("Serial Number: {0}".format(dev.Name))
+                # These are individual devices
+                # This is what we use to map in to the USB_DEVICES
+
+
+                usb_info_dict = {}
+
+                #Serial Number
+                usb_info_dict['Serial Number'] = str(dev.Name)
+                usb_info_dict['Vendor'] = vendor
+                usb_info_dict['Product'] = product
+                usb_info_dict['Revision'] = rev
+
+
+                # Get all the sub values
                 values = self.regapi.reg_yield_values('SYSTEM', dev, given_root=dev)
                 debug.info("Device Values")
                 for val in values:
                     key_name = val[0]
                     key_data = val[1]
-                    debug.info('Key: {0}\t Data: {1}'.format(key_name, key_data))
+                    usb_info_dict[key_name] = key_data
+
+                # Now get the Drive letters if we can
+
+
+                # Get the last written key for each device
+
+                serial_number = usb_info_dict['Serial Number']
+                usb_subkeys = self.regapi.reg_get_all_subkeys('SYSTEM', USB_PATH, given_root=usb_key)
+                for a in usb_subkeys:
+                    subs = self.regapi.reg_get_all_subkeys('SYSTEM', a, given_root=a)
+                    for s in subs:
+                        if serial_number.split('&')[0] == s.Name:
+                            usb_info_dict['Device Last Plugged In'] = s.LastWriteTime
+
+
+                # Check if the current NTUSER.dat file contains the MountPoints2 entry
+                # If yes user = this one else user = unknown
+
+
+
+            results['USB_DEVICES'].append(usb_info_dict)
+
+
 
 
 
@@ -104,9 +151,10 @@ class RegThings(common.AbstractWindowsCommand):
             if not result['exists']:
                 outfd.write('USBSTOR Not found in SYSTEM Hive\n')
             else:
-                for k in result['subkeys']:
-                    outfd.write('Found USB Drive: ')
-                    outfd.write(k)
+                for usbdev in result['USB_DEVICES']:
+                    outfd.write('Found USB Drive: \n')
+                    for k, v in usbdev.iteritems():
+                        outfd.write('{0}\t{1}\n'.format(k, v))
 
 
         outfd.write('\n')
