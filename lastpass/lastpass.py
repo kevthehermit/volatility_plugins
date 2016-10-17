d2ebbd569ff0805ef79794cab59286fa4c759faf
kevthehermit <kevthehermit@gmail.com>
2016-10-17 11:44:34 +0100

    New Signatures and PrivateKey

--- lastpass/lastpass.py
+++ lastpass/lastpass.py
@@ -21,11 +21,16 @@ except ImportError:
 
 
 signatures = {
-    'lastpass_struct_a' : 'rule lastpass_strcuta {strings: $a = /{"reqinfo":.*"lplanguage":""}/ condition: $a}\nrule lastpass_strcutb {strings: $a = /"tld":".*?","unencryptedUsername":".*?","realmmatch"/ condition: $a}'
+    'lastpass_struct_a': 'rule lastpass_strcuta {strings: $a = /{"reqinfo":.*"lplanguage":""}/ condition: $a}\n'
+                         'rule lastpass_strcutb {strings: $a = /"tld":".*?","unencryptedUsername":".*?","realmmatch"/ condition: $a}\n'
+                         'rule lastpass_strcutc {strings: $a = /{"cmd":"save"(.*?)"tld":"(.*?)"}/ condition: $a}\n'
+                         'rule lastpass_strcutd {strings: $a = /"realurl":"(.*?)"domains":"(.*?)"/ condition: $a}\n'
+                         'rule lastpass_strcute {strings: $a = /{"cmd":"save"(.*?)"formdata"(.*?)}/ condition: $a}\n'
+                         'rule lastpass_priv1 {strings: $a = /LastPassPrivateKey<(.*?)>LastPassPrivateKey/ condition: $a}'
 }
 
 config = conf.ConfObject()
-config.add_option('CONFSIZE', short_option='C', default=2000,
+config.add_option('CONFSIZE', short_option='C', default=4000,
                            help ='Config data size',
                            action ='store', type='int')
 config.add_option('YARAOFFSET', short_option='Y', default=0,
@@ -61,69 +66,174 @@ class LastPass(taskmods.PSList):
 
 
     def clean_json(self, raw_data):
-        # We deliberatly pull in too much data to make sure we get it all.
+        # We deliberately pull in too much data to make sure we get it all.
         # Now parse it out again
 
+        if raw_data.startswith('LastPassPrivate'):
+            pattern = 'LastPassPrivateKey<(.*?)>LastPassPrivateKey'
+            key_data = re.search(pattern, raw_data).group(0)
+            if not any(substring in key_data for substring in ['"+a+"', 'indexOf']):
+                return self.string_clean_hex(key_data)
 
-        if raw_data.startswith("\"tld"):
-            pattern = '"tld":".*?","unencryptedUsername":".*?","realmmatch"'
-            val_type = 'username'
         else:
-            pattern = '{"reqinfo":.*?"lplanguage":""}'
-            val_type = 'password'
 
-        match = re.search(pattern, raw_data)
-        real_data = self.string_clean_hex(match.group(0))
+            if raw_data.startswith("{\"cmd"):
+                if 'formdata' in raw_data:
+                    pattern = '{"cmd":"save"(.*?)"formdata"(.*?)}'
+                    val_type = 'mixedform'
+                else:
+                    pattern = '{"cmd":"save"(.*?)"tld":"(.*?)"}'
+                    val_type = 'mixed'
 
-        if val_type == 'username':
-            vars = real_data.split(',')
-            tld = vars[0].split(':')[1].strip('"')
-            username = vars[1].split(':')[1].strip('"')
-            return {'type': 'username', 'username': username, 'tld': tld}
-        else:
-            # Try to parse as json
-            try:
-                json_data = json.loads(real_data)
-                tld = json_data['domains']
-                password = json_data['value']
-            except ValueError:
-                # Json fails manual parse
-                password = re.search('"value":"(.*?)"', real_data).group(0).split(':')[-1].strip('"')
-                tld = re.search('"domains":"(.*?)"', real_data).group(0).split(':')[-1].strip('"')
-            return {'type': 'password', 'password': password, 'tld': tld}
+            elif raw_data.startswith("\"realurl"):
+                pattern = '"realurl":"(.*?)"domains":"(.*?)"'
+                val_type = 'mixed'
+
+            elif raw_data.startswith("\"tld"):
+                pattern = '"tld":".*?","unencryptedUsername":".*?","realmmatch"'
+                val_type = 'username'
+
+            elif raw_data.startswith('{"reqinfo"'):
+                pattern = '{"reqinfo":.*?"lplanguage":""}'
+                val_type = 'password'
+            else:
+                pass
+
+            match = re.search(pattern, raw_data)
+            real_data = self.string_clean_hex(match.group(0))
+
+            if val_type == 'username':
+                vars = real_data.split(',')
+                tld = vars[0].split(':')[1].strip('"')
+                username = vars[1].split(':')[1].strip('"')
+                return {'type': 'username', 'username': username, 'tld': tld}
+
+            elif val_type == 'mixedform':
+                if '"tld":"' in real_data:
+                    # Try to parse as json
+                    try:
+                        json_data = json.loads(real_data)
+                        tld = json_data['tld']
+                        password = json_data['password']
+                        username = json_data['username']
+                        clean_data = {'type': 'mixed', 'username': username, 'tld': tld, 'password': password}
+                    except ValueError:
+                        # Json fails manual parse
+                        tld = re.search('"tld":"(.*?)"', real_data)
+                        if not tld:
+                            tld = re.search('"domains":"(.*?)"', real_data)
+                        if tld:
+                            tld = tld.group(0).split(':')[-1].strip('"')
+                        else:
+                            tld = 'unknown'
+                        try:
+                            password = re.search('"password":"(.*?)"', real_data).group(0).split(':')[-1].strip('"')
+                        except:
+                            password = 'Unknown'
+                        try:
+                            username = re.search('"username":"(.*?)"', real_data).group(0).split(':')[-1].strip('"')
+                        except:
+                            username = 'Unknown'
+                        clean_data = {'type': 'mixed', 'username': username, 'tld': tld, 'password': password}
+
+                else:
+                    tld = re.search('"url":"(.*?)"', real_data).group(0).split(':')[-1].strip('"')
+                    username = re.search('login(.*?)text', real_data).group(0).split('\\t')[1]
+                    password = re.search('password(.*?)password', real_data).group(0).split('\\t')[1]
+                    clean_data = {'type': 'mixed', 'username': username, 'tld': tld, 'password': password}
+
+                return clean_data
+
+            elif val_type == 'mixed':
+                # Try to parse as json
+                try:
+                    json_data = json.loads(real_data)
+                    tld = json_data['tld']
+                    password = json_data['password']
+                    username = json_data['username']
+                    clean_data = {'type': 'mixed', 'username': username, 'tld': tld, 'password': password}
+                except ValueError:
+                    # Json fails manual parse
+                    tld = re.search('"tld":"(.*?)"', real_data)
+                    if not tld:
+                        tld = re.search('"domains":"(.*?)"', real_data)
+
+                    if tld:
+                        tld = tld.group(0).split(':')[-1].strip('"')
+                    else:
+                        tld = 'unknown'
+
+                    try:
+                        password = re.search('"password":"(.*?)"', real_data).group(0).split(':')[-1].strip('"')
+                    except:
+                        password = 'Unknown'
+                    try:
+                        username = re.search('"username":"(.*?)"', real_data).group(0).split(':')[-1].strip('"')
+                    except:
+                        username = 'Unknown'
+
+                    clean_data = {'type': 'mixed', 'username': username, 'tld': tld, 'password': password}
+            else:
+                # Try to parse as json
+                try:
+                    json_data = json.loads(real_data)
+                    tld = json_data['domains']
+                    password = json_data['value']
+                    clean_data = {'type': 'password', 'password': password, 'tld': tld}
+                except ValueError:
+                    # Json fails manual parse
+                    password = re.search('"value":"(.*?)"', real_data).group(0).split(':')[-1].strip('"')
+                    tld = re.search('"domains":"(.*?)"', real_data).group(0).split(':')[-1].strip('"')
+                    clean_data = {'type': 'password', 'password': password, 'tld': tld}
+
+            return clean_data
 
     def render_text(self, outfd, data):
         """ Required: Parse data and display """
-        outfd.write("LastPass Results")
+        outfd.write("Searching for LastPass Signatures\n")
 
         rules = yara.compile(sources=signatures)
 
         results = {}
+        priv_keys = []
 
         for task, address in data:  # iterate the yield values from calculate()
-            outfd.write('Checking Process: {0} ({1})\n'.format(task.ImageFileName, task.UniqueProcessId))
+            outfd.write('Found pattern in Process: {0} ({1})\n'.format(task.ImageFileName, task.UniqueProcessId))
             proc_addr_space = task.get_process_address_space()
             raw_data = proc_addr_space.read(address + self._config.YARAOFFSET, self._config.CONFSIZE)
-            clean_data = self.clean_json(raw_data)
-
-            if clean_data['tld'] in results:
-                if 'username' in clean_data:
-                    results[clean_data['tld']]['username'] = clean_data['username']
-                if 'password' in clean_data:
-                    results[clean_data['tld']]['password'] = clean_data['password']
-            else:
-                if 'username' in clean_data:
-                    username = clean_data['username']
+            if raw_data:
+                clean_data = self.clean_json(raw_data)
+                if not clean_data:
+                    continue
+                if 'PrivateKey' in clean_data:
+                    priv_keys.append(clean_data)
                 else:
-                    username = 'Unknown'
-                if 'password' in clean_data:
-                    password = clean_data['password']
-                else:
-                    password = 'Unknown'
-                results[clean_data['tld']] = {'username': username, 'password': password}
+                    # If we already created the dict
+                    if clean_data['tld'] in results:
+                        if 'username' in clean_data:
+                            if results[clean_data['tld']]['username'] == 'Unknown':
+                                results[clean_data['tld']]['username'] = clean_data['username']
+                        if 'password' in clean_data:
+                            if results[clean_data['tld']]['password'] == 'Unknown':
+                                results[clean_data['tld']]['password'] = clean_data['password']
+                    # Else create the dict
+                    else:
+                        if 'username' in clean_data:
+                            username = clean_data['username']
+                        else:
+                            username = 'Unknown'
+                        if 'password' in clean_data:
+                            password = clean_data['password']
+                        else:
+                            password = 'Unknown'
+                        results[clean_data['tld']] = {'username': username, 'password': password}
 
         for k, v in results.iteritems():
             outfd.write("\nFound LastPass Entry for {0}\n".format(k))
             outfd.write('UserName: {0}\n'.format(v['username']))
             outfd.write('Pasword: {0}\n'.format(v['password']))
         outfd.write('\n')
+
+        for key in priv_keys:
+            outfd.write('\nFound Private Key\n')
+            outfd.write('{0}\n'.format(key))
